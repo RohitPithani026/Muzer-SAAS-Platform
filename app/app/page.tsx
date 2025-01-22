7e9c7bd29c9a2a2c0f998b254504a01e1d6c3b52
RohitPithani026 <rohitpithani13@gmail.com>
2025-01-22 07:37:32 +0530

    Started Muzer App

---
+++ app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+console.log(process.env.GOOGLE_CLIENT_ID)
+console.log(process.env.GOOGLE_CLIENT_SECRET)
+export default function Home() {
+  return (
+    <main>
+      hi there
+    </main>
+  );
+}
